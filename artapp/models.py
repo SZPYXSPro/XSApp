2a4c20bcdb1f1ad4c0d1aa3179ac8594749414d0
stephency <stephency@foxmail.com>
2018-07-03 12:04:19 +0800

    '添加了models'

--- artapp/models.py
+++ artapp/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
+
 # Create your models here.
+class ArtTag(models.Model):
+    title = models.CharField(max_length=20, verbose_name='作品类别', unique=True, blank=False, db_index=True)
+
+    # auto_now:每次修改的时间
+    # auto_now_add:第一次添加的时间
+    add_time = models.DateTimeField(verbose_name='添加的时间', auto_now_add=True)
+    modify_time = models.DateTimeField(verbose_name='修改的时间', auto_now=True)
+
--- artapp/views.py
+++ artapp/views.py
@@ -1,9 +1,15 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+
 # Create your views here.
 def index(request):
+    '''
+    Desc:
+        首页
+    :param request:request请求对象,包含请求头等信息
+    :return:
+        返回list.html
+    '''
 
-
-
-    return HttpResponse('nihao')
+    return render(request, 'art/list.html', context={'name': 'stephen', 'age': 20})
